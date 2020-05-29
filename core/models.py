af2f971d0d388b5b2672eb53666971972875a7c9
João Pedro Limão <jplimao12@gmail.com>
2020-05-29 06:32:32 -0300

    Creating model and first migration

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    text = models.CharField(max_length=1000)
+
+
+class Email(models.Model):
+    text = models.EmailField()
