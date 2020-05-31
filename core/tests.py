e93e55f35125ebaa65e91d2574cc2c095d208ba3
João Pedro Limão <jplimao12@gmail.com>
2020-05-31 09:58:37 -0300

    test: Creating test to check if email is unique

--- core/tests.py
+++ core/tests.py
@@ -11,6 +11,7 @@ class HomePageTests(TestCase):
         self.client = Client()
         self.url = reverse('core:home')
         self.template = 'home.html'
+        self.test_email = 'email.example@gmail.com'
     
     def test_home_url(self):
         self.assertEquals(resolve(self.url).func.view_class, Home)
@@ -21,18 +22,21 @@ class HomePageTests(TestCase):
         self.assertTemplateUsed(response, self.template)
     
     def test_home_add_email(self):
-        test_email = 'email.example@gmail.com'
-
         response = self.client.post(self.url, {
-            'text': test_email
+            'text': self.test_email
         })
 
         email = Email.objects.get(id=1)        
         self.assertEquals(response.status_code, 302)
-        self.assertEquals(email.text, test_email)
-
-        
-
+        self.assertEquals(email.text, self.test_email)
     
+    def test_unique_email(self):
+        for i in range(2):
+            self.client.post(self.url, {
+                'text': self.test_email
+            })
 
-
+        try:
+            email = Email.objects.get(id=2)
+        except Email.DoesNotExist as err:
+            self.assertTrue(True)
