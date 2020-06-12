cd36835add5ee7fd784e49c77cc4efb613f033e9
João Pedro Limão <jplimao12@gmail.com>
2020-06-12 16:33:13 -0300

    study : If logic to class fuctions

--- celeryEmails/middleware.py
+++ celeryEmails/middleware.py
@@ -13,7 +13,7 @@ class AddEmailMiddleware:
     def process_view(self, request, view_func, view_args, view_kwargs):
         if view_func.view_class == Home:
             print(f'debug -> view_class is Home')
-            # print(f'debug -> method: {request.method}')
+
             # POST is being called here to create a new e-mail!
             if request.method == 'POST':
                 print(f'debug -> request method is POST')
